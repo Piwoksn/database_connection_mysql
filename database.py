f2110879125b4fd40e3b81cc44f86f6ae2910aee
Piwoksn <piwoksn@gmail.com>
2024-09-18 14:11:02 +0100

    Added table to database

--- database.py
+++ database.py
@@ -4,7 +4,17 @@ db = mysql.connect(
     host = "localhost",
     db = "mccoy",
     user= "root",
-    passwd= input("Please Enter Password")
+    passwd= input("Please Enter Password: ")
 )
 
-
+if db.is_connected:
+    print("Databae is connected")
+    
+    cursor = db.cursor()
+    # Created database Peple
+    cursor.execute(
+        "CREATE TABLE People (Name varchar(50), Age int,Location varchar(50))"
+    )
+    
+else:
+    print("Not Connected")
